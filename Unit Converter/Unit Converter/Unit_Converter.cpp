c3fba69d6d8ec458d19a216a180425e407070d73
JT Gulick <jtgulick1@gmail.com>
2025-02-04 18:01:46 -0500

    Finished Weight

--- Unit Converter/Unit Converter/Unit_Converter.cpp
+++ Unit Converter/Unit Converter/Unit_Converter.cpp
@@ -13,7 +13,13 @@ void Conversion(float con, int section) { // con == medium of each section
 
 	}
 	else if (section == 2) {
-
+		cout << "lbs: " << con << endl;
+		cout << "kg: " << con / 2.2046244 << endl;
+		cout << "grams: " << con / 0.0022046 << endl;
+		cout << "tons: " << con / 2000 << endl;
+		cout << "ounce: " << con * 16 << endl;
+		cout << "Imperial Ton: " << con / 2240 << endl;
+		cout << "Milligram: " << con * 453600 << endl;
 	}
 	else if (section == 3) {
 		cout << "Celcius: " << con << endl;
@@ -26,12 +32,120 @@ void Conversion(float con, int section) { // con == medium of each section
 	cin >> ext;
 }
 
-void Distance() { // WIP
-
+void Distance() { // con = Cm
+	system("cls");
+	string help = "";
+	cout << "What unit do you have\n(1) Cm\n(2) Feet\n(3) Inches\n(4) Meters\n(5) Yards\n(6) Miles\n(7) Kilometer\n(8) Millimeter\n(9) Micrometer\n(0) Nautical Miles\n";
+	cin >> help;
+	if (help == "1") { // lbs - con
+		cout << "How Much?\n";
+		cin >> help;
+		float lbs = (stof(help));
+		Conversion(lbs, 2);
+	}
+	else if (help == "2") { // kgs
+		cout << "How Much?\n";
+		cin >> help;
+		float lbs = (stof(help) * 2.2046244f);
+		Conversion(lbs, 2);
+	}
+	else if (help == "3") { // grams
+		cout << "How Much?\n";
+		cin >> help;
+		float lbs = (stof(help) / 453.5920865);
+		Conversion(lbs, 2);
+	}
+	else if (help == "4") { // tons
+		cout << "How Much?\n";
+		cin >> help;
+		float lbs = (stof(help) * 2000);
+		Conversion(lbs, 2);
+	}
+	else if (help == "5") { // ounce
+		cout << "How Much?\n";
+		cin >> help;
+		float lbs = (stof(help) / 16);
+		Conversion(lbs, 2);
+	}
+	else if (help == "6") { // troy ounce
+		cout << "How Much?\n";
+		cin >> help;
+		float lbs = (stof(help) * 2240);
+		Conversion(lbs, 2);
+	}
+	else if (help == "7") { // milligram
+		cout << "How Much?\n";
+		cin >> help;
+		float lbs = (stof(help) / 453600);
+		Conversion(lbs, 2);
+	}
+	else if (help == "8") { // ounce
+		cout << "How Much?\n";
+		cin >> help;
+		float lbs = (stof(help) / 16);
+		Conversion(lbs, 2);
+	}
+	else if (help == "9") { // troy ounce
+		cout << "How Much?\n";
+		cin >> help;
+		float lbs = (stof(help) * 2240);
+		Conversion(lbs, 2);
+	}
+	else if (help == "0") { // milligram
+		cout << "How Much?\n";
+		cin >> help;
+		float lbs = (stof(help) / 453600);
+		Conversion(lbs, 2);
+	}
 }
 
-void Weight() { // WIP
-
+void Weight() { //con = lbs
+	system("cls");
+	string help = "";
+	cout << "What unit do you have\n(1) lbs\n(2) kg\n(3) grams\n(4) US tons\n(5) ounce\n(6) Imperial Ton\n(7) Milligram\n";
+	cin >> help;
+	if (help == "1") { // lbs - con
+		cout << "How Much?\n";
+		cin >> help;
+		float lbs = (stof(help));
+		Conversion(lbs, 2);
+	}
+	else if (help == "2") { // kgs
+		cout << "How Much?\n";
+		cin >> help;
+		float lbs = (stof(help) * 2.2046244f);
+		Conversion(lbs, 2);
+	}
+	else if (help == "3") { // grams
+		cout << "How Much?\n";
+		cin >> help;
+		float lbs = (stof(help) / 453.5920865);
+		Conversion(lbs, 2);
+	}
+	else if (help == "4") { // tons
+		cout << "How Much?\n";
+		cin >> help;
+		float lbs = (stof(help) * 2000);
+		Conversion(lbs, 2);
+	}
+	else if (help == "5") { // ounce
+		cout << "How Much?\n";
+		cin >> help;
+		float lbs = (stof(help) / 16);
+		Conversion(lbs, 2);
+	}
+	else if (help == "6") { // troy ounce
+		cout << "How Much?\n";
+		cin >> help;
+		float lbs = (stof(help) * 2240);
+		Conversion(lbs, 2);
+	}
+	else if (help == "7") { // milligram
+		cout << "How Much?\n";
+		cin >> help;
+		float lbs = (stof(help) / 453600);
+		Conversion(lbs, 2);
+	}
 }
 
 void Temp() { //con == Cel
@@ -63,7 +177,7 @@ int main() {
 	while (true) {
 		system("cls");
 		cout << "Welcome to the Unit Converter" << endl;
-		cout << "(1) Distance\n(2) Weight\n(3) Tempature\n(4) Exit Application";
+		cout << "(1) Distance\n(2) Weight\n(3) Tempature\n(4) Exit Application\n";
 		string choice = "";
 		cin >> choice;
 		if (choice == "1") {
